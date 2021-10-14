78d4fbca43ccfb9b91c5275b4bc572290df32dad
pater97 <giuseppepater997@gmail.com>
2021-10-14 14:44:20 +0200

    impostazione base esercizio

---
+++ assets/javascript/main.js
@@ -0,0 +1,12 @@
+//CONSEGNA:
+/*
+MAIL -->
+- Chiedi all’utente la sua email,
+- controlla che sia nella lista di chi può accedere,
+- stampa un messaggio appropriato sull’esito del controllo.
+DADI -->
+- generare un numero random da 1 a 6, sia per l'utente che per il pc
+- stabilire il vincitore in base al punteggio più alto.
+BONUS -->
+- usare un input e un bottone per inserire la mail per poi mostrare i contenuti sulla pagina
+*/
