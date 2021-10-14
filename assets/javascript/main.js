87155a2b9276a45fc0aa7cf3a2eb5fe26ff0a166
pater97 <giuseppepater997@gmail.com>
2021-10-14 16:11:50 +0200

    test 1 e-mail

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,8 +16,23 @@ BONUS -->
 MAIL:
 - 1 creare un Array con la lista di e-mail abilitate all'accesso.
 - 2 chiedere attraverso un prompt all'utente di inserire la sua e-mail per il confronto
-- 3 tramite l'uso delle condizioni: se la mail è all'interno dell'Arrey allora fare apparire un messaggio di conferma, altrimenti no.
+- 3 definire le variabili di inserimento in html
+- 4 tramite l'uso delle condizioni: se la mail è all'interno dell'Arrey allora fare apparire un messaggio di conferma, altrimenti no.
 */
 
+//1
 const approvateUser = ["tizio.caio@gmail.com", "pinco.pallino@gmail.com", "lorem.ipsum@gmail.com", "mario.rossi@gmail.com"];
-console.log(approvateUser);
+console.log(approvateUser); //test
+//2
+const emailUser = prompt(`qual'è la tua email?`)
+//3
+const message = document.querySelector(".message")
+//4 
+
+if (approvateUser == emailUser) {
+    let answer = `congratulazioni la tua e-mail è abilitata.`
+    message.innerHTML = answer
+} else {
+    let answer = `la tua e-mail non è abilitata`
+    message.innerHTML = answer
+}
