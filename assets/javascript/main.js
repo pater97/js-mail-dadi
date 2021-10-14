7b53bb6249ed00be31bee8e75ff06f6b00c12607
pater97 <giuseppepater997@gmail.com>
2021-10-14 17:46:46 +0200

    esercizio base completo

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -16,23 +16,55 @@ BONUS -->
 MAIL:
 - 1 creare un Array con la lista di e-mail abilitate all'accesso.
 - 2 chiedere attraverso un prompt all'utente di inserire la sua e-mail per il confronto
-- 3 definire le variabili di inserimento in html
-- 4 tramite l'uso delle condizioni: se la mail è all'interno dell'Arrey allora fare apparire un messaggio di conferma, altrimenti no.
+- 3 definire le variabili di inserimento in html e la variabile sentinella
+- 4 fare ciclare l'array
+- 5 stabilire con una variabile sentinella il valore vero o falso in base a se l'email cercata è presente
+- 6 inserire il messaggio in html
 */
 
 //1
 const approvateUser = ["tizio.caio@gmail.com", "pinco.pallino@gmail.com", "lorem.ipsum@gmail.com", "mario.rossi@gmail.com"];
-console.log(approvateUser); //test
 //2
 const emailUser = prompt(`qual'è la tua email?`)
 //3
 const message = document.querySelector(".message")
-//4 
-
-if (approvateUser == emailUser) {
-    let answer = `congratulazioni la tua e-mail è abilitata.`
-    message.innerHTML = answer
-} else {
-    let answer = `la tua e-mail non è abilitata`
-    message.innerHTML = answer
+let find = false
+//4
+for (let i = 0; i < approvateUser.length; i++) {
+    if (approvateUser[i] == emailUser) { //5
+        find = true
+    }
 }
+//6
+if (find == true) {
+    message.innerHTML = `la tua e-mail è abilitata`
+} else {
+    message.innerHTML = `la tua e-mail non è abilitata`
+}
+
+
+
+
+//COSA FARE: 
+/* 
+DADI
+- 1 definire le variabili per il numero del pc e dell'utente con i relativi numeri 
+- 2 inserire quest'ultimi nell'html
+- 3 definire il vincitore con una condizione
+*/
+//1
+const pcNumber = Math.floor(Math.random() * 5) + 1;
+const userNumber = Math.floor(Math.random() * 5) + 1;
+const result = document.querySelector(".result")
+//2
+document.querySelector(`.pcNumber`).innerHTML = pcNumber
+document.querySelector(`.userNumber`).innerHTML = userNumber
+//3
+if (pcNumber > userNumber) {
+    let win = (`il mio numero è più alto, ho vinto`)
+    result.innerHTML = win
+} else {
+    let win = (`congratulazioni, il tuo numero è più alto, hai vinto!`)
+    result.innerHTML = win
+}
+
